8ac1956cab458cb4333ff1c32fb7e5523457d47b
pungkiilham <pungkiilham92@example.com>
2025-07-18 21:24:05 +0700

    update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,9 +25,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'username' => fake()->userName(),
+            // 'email' => fake()->unique()->safeEmail(),
+            // 'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('123'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -38,7 +39,7 @@ class UserFactory extends Factory
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            // 'email_verified_at' => null,
         ]);
     }
 }
